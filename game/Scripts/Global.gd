3be74a62c3c7bb7840eb6fc998d192363adfd5af
PHGomides <gomidesmoraesp@gmail.com>
2024-10-20 19:05:58 -0300

    Adicionando temporizador

--- game/Scripts/Global.gd
+++ game/Scripts/Global.gd
@@ -1,10 +1,9 @@
 extends Node
 
-var tempo := 60
 var vida_player1 := 200
 var vida_player2 := 200
 
-# Called when the node enters the scene tree for the first time.
+# Called when the node enters the  scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
