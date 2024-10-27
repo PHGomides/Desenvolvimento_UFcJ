5ba0f457dfb05e91e23d94d908599d9d85cae703
PHGomides <gomidesmoraesp@gmail.com>
2024-10-27 16:36:44 -0300

    Adicionando barra de vida

---
+++ game/Scripts/HUD/Tempo_hud.gd
@@ -0,0 +1,24 @@
+extends Control
+
+@onready var tempo_label = $MarginContainer/Tempo_container/Tempo_label as Label
+@onready var clocktimer = $clocktimer as Timer
+@export_range(0,90) var default_seconds := 90
+var seconds = 0
+signal time_is_up()
+  # Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	tempo_label.text = str("%02d" % default_seconds)
+	reset_clock_timer()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+  
+
+
+func _on_clocktimer_timeout() -> void:
+	seconds -= 1
+	tempo_label.text = str("%02d" % seconds)
+	
+func  reset_clock_timer():
+	seconds = default_seconds
