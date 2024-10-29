b0290102d980d4976c898abeaa40acfcb5eb35b1
PHGomides <gomidesmoraesp@gmail.com>
2024-10-28 22:34:49 -0300

    Arrumando TIMER em todos os mapas adicionado

--- game/Scripts/HUD/gerencia_hud.gd
+++ game/Scripts/HUD/gerencia_hud.gd
@@ -5,10 +5,24 @@ extends Control
 @onready var clocktimer = $clocktimer as Timer
 
 var seconds = 0
+
 @export_range(0,90) var default_seconds := 90
+@onready var ef_bv_player_1: TextureProgressBar = $EF_BarraVidaPlayer1
+@onready var bv_player_1: TextureProgressBar = $BarraVidaPlayer1
+
+@onready var ef_bv_player_2: TextureProgressBar = $EF_BarraVidaPlayer2
+@onready var bv_player_2: TextureProgressBar = $BarraVidaPlayer2
+
+@export var vidaMAX: int
+@export var vida_player1 = 0 : set = _set_vida
+@export var vida_player2 = 0 : set = _set_vida
 
 signal time_is_up()
   # Called when the node enters the scene tree for the first time.
+
+func _set_vida(nova_vida):
+	return	
+	
 func _ready() -> void:
 	tempo_label.text = str("%02d" % default_seconds)
 	reset_clock_timer()
