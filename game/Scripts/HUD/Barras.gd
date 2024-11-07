86408d395e97e2d56a16a661ab39430feb6e5d43
PHGomides <gomidesmoraesp@gmail.com>
2024-11-07 15:16:31 -0300

    Corrigindo barra de vida

--- game/Scripts/HUD/Barras.gd
+++ game/Scripts/HUD/Barras.gd
@@ -30,11 +30,14 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	# Atualiza as barras de vida com a vida atual dos jogadores
 	if Global.player1 and Global.player2:
-		if ((Global.player1.vida != vida_1) or (Global.player2.vida != vida_2) ):
-			_update_health_bars(Global.player1.vida,Global.player2.vida)
-			_update_power_bars(Global.player1.power,Global.player2.power)
-		if ((Global.player1.power != power_1) or (Global.player2.power != power_2) ):
-			_update_power_bars(Global.player1.power,Global.player2.power)
+		if (Global.player1.vida != vida_1):
+			_set_vida1(Global.player1.vida)
+		if (Global.player2.vida != vida_2):
+			_set_vida2(Global.player2.vida)
+		if (Global.player1.power != power_1):
+			_set_power1(Global.player1.power)
+		if (Global.player2.power != power_2):
+			_set_power2(Global.player2.power)
 	
 
 func _init_vida():
@@ -42,31 +45,39 @@ func _init_vida():
 	for bar in [bv_player_1, bv_player_2, ef_bv_player_1, ef_bv_player_2]:
 		bar.max_value = vidaMAX
 		bar.value = vidaMAX
+	
+	
 
-func _update_health_bars(nova_vida1 : int, nova_vida2 : int):
-	# Acessa as vidas dos jogadores diretamente
+func _set_vida1(nova_vida1 : int):
 	nova_vida1 = clamp(nova_vida1, 0, vidaMAX)  # Restringe o valor entre 0 e vidaMAX
 	vida_1 = nova_vida1
 	
-	nova_vida2 = clamp(nova_vida2, 0, vidaMAX)  # Restringe o valor entre 0 e vidaMAX
-	vida_2 = nova_vida2
-	
 	move_tween(bv_player_1, vida_1, 0.1)
-	move_tween(bv_player_2, vida_2, 0.1)
 	timer_barra.start(1.0)
 	await timer_barra.timeout
 	move_tween(ef_bv_player_1, vida_1, 0.1)
+	
+	
+func _set_vida2(nova_vida2 : int):
+	nova_vida2 = clamp(nova_vida2, 0, vidaMAX)  # Restringe o valor entre 0 e vidaMAX
+	vida_2 = nova_vida2
+	
+	move_tween(bv_player_2, vida_2, 0.1)
+	timer_barra.start(1.0)
+	await timer_barra.timeout
 	move_tween(ef_bv_player_2, vida_2, 0.1)
 
-func _update_power_bars(novo_power1 : int, novo_power2 : int):
-# Acessa o poder dos jogadores diretamente
+func _set_power1(novo_power1 : int):
 	novo_power1 = clamp(novo_power1, 0, powerMAX)  # Restringe o valor entre 0 e vidaMAX
 	power_1 = novo_power1
 	
+	move_tween(bp_power_player_1, power_1, 0.2)
+
+
+func _set_power2(novo_power2 : int):
 	novo_power2 = clamp(novo_power2, 0, powerMAX)  # Restringe o valor entre 0 e vidaMAX
 	power_2 = novo_power2
 	
-	move_tween(bp_power_player_1, power_1, 0.2)
 	move_tween(bp_power_player_2, power_2, 0.2)
 	
 func move_tween(node, nova_vida, speed):
